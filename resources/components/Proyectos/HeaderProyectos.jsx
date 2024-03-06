3d3a5fac314d8821c8448544ac701c1e38a6091b
nahuek4 <nahuek.75@gmail.com>
2024-03-06 02:21:05 -0300

    prueba2

--- resources/components/Proyectos/HeaderProyectos.jsx
+++ resources/components/Proyectos/HeaderProyectos.jsx
@@ -5,7 +5,7 @@ const HeaderProyectos = () => {
         <div>
             <div className="headerProyectoContainer">
                 <div className="tituloContainerHeaderProyecto">
-                    <h1>Nuestros <span className='spanRosa'></span></h1>
+                    <h1>Nuestros <span className='spanRosa'>asd</span></h1>
                     <p>¡Conocé algunos de los proyectos de los que fuimos parte!</p>
                 </div>
             </div>
