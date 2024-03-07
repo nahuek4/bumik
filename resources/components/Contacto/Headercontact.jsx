5cb1020e36f6378c30bbe97f59e725a3fbb613e4
nahuek4 <nahuek.75@gmail.com>
2024-03-07 20:30:58 -0300

    fix css

--- resources/components/Contacto/Headercontact.jsx
+++ resources/components/Contacto/Headercontact.jsx
@@ -7,9 +7,11 @@ const Headercontact = () => {
             <div className="headerContactContainer">
                 <div className="headerContactTitulo">
                     <h1>¡Trabajemos <span className='spanRosa'>Juntos</span>!</h1>
-                    <p>¿Estas buscando un diseño único y atractivo para tu marca?</p>
-                    <p>Con nuestros servicios, tu marca será reconocida y respetada en su industria. </p>
-                    <p>¡Contáctanos hoy para obtener un diseño de marca de calidad!</p>
+                    <div className="headerTextP">
+                        <p>¿Estas buscando un diseño único y atractivo para tu marca?</p>
+                        <p>Con nuestros servicios, tu marca será reconocida y respetada en su industria. </p>
+                        <p>¡Contáctanos hoy para obtener un diseño de marca de calidad!</p>
+                    </div>
                 </div>
             </div>
             <Form />
