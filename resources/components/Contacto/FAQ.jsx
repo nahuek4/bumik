691a29cd90131d5ebc0218adb38d846b593b508d
nahuek4 <nahuek.75@gmail.com>
2024-03-08 02:59:12 -0300

    FAQS

---
+++ resources/components/Contacto/FAQ.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react';
+
+const FAQ = () => {
+    const [datos, setDatos] = useState([]);
+    const [selected, setSelected] = useState(null);
+
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null);
+        }
+
+        setSelected(i);
+    }
+    return (
+        <div>
+            <div className="faqContainerComp">
+                <div className="faqContain">
+                    <div className="tittleFaq">
+                        <h1>Preguntas <span className='spanRosa'>Frecuentes</span></h1>
+                        <p>FAQS Generales</p>
+                    </div>
+                    <div className="item">
+                        <div className="tittle" onClick={() => toggle(i)}>
+                            <div className="vacio"></div>
+                            <h1>¿Puedo diseñar solo el logo de mi marca?</h1>
+                            <span>{selected === i ? '-' : '+'}</span>
+                        </div>
+                        <div className={selected === i ? "content show" : "content"}><p>{item.descripcion}</p></div>
+                    </div>
+                    <div className="item">
+                        <div className="tittle" onClick={() => toggle(i)}>
+                            <div className="vacio"></div>
+                            <h1>¿Cuáles son los métodos de pago de los servicios?</h1>
+                            <span>{selected === i ? '-' : '+'}</span>
+                        </div>
+                        <div className={selected === i ? "content show" : "content"}><p>{item.descripcion}</p></div>
+                    </div>
+                    <div className="item">
+                        <div className="tittle" onClick={() => toggle(i)}>
+                            <div className="vacio"></div>
+                            <h1>¿Cuánto demora del proceso de Branding?</h1>
+                            <span>{selected === i ? '-' : '+'}</span>
+                        </div>
+                        <div className={selected === i ? "content show" : "content"}><p>{item.descripcion}</p></div>
+                    </div>
+                    <div className="item">
+                        <div className="tittle" onClick={() => toggle(i)}>
+                            <div className="vacio"></div>
+                            <h1>¿En donde dan servicio?</h1>
+                            <span>{selected === i ? '-' : '+'}</span>
+                        </div>
+                        <div className={selected === i ? "content show" : "content"}><p>{item.descripcion}</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ
--- resources/components/Contacto/Headercontact.jsx
+++ resources/components/Contacto/Headercontact.jsx
@@ -6,7 +6,7 @@ const Headercontact = () => {
         <div>
             <div className="headerContactContainer">
                 <div className="headerContactTitulo">
-                    <h1>¡Trabajemos <span className='spanRosa'>Juntos</span>!</h1>
+                    <h1>¡Trabajemos <span className='spanVerde'>Juntos</span>!</h1>
                     <div className="headerTextP">
                         <p>¿Estas buscando un diseño único y atractivo para tu marca?</p>
                         <p>Con nuestros servicios, tu marca será reconocida y respetada en su industria. </p>
