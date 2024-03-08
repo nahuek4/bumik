a2c7b568af65f2cdd9bfc570768b065e9e074920
nahuek4 <nahuek.75@gmail.com>
2024-03-08 03:17:35 -0300

    fix label problems

--- resources/components/Contacto/Form.jsx
+++ resources/components/Contacto/Form.jsx
@@ -16,24 +16,19 @@ const Form = () => {
                         <div className="formNombreApellido">
                             {/* nombre */}
                             <div className="labelNombre">
-                                <label htmlFor="nombre">Nombre</label>
-                                <input type="text" {...register("nombre")} />
+                                <label htmlFor="nombre">Nombre <input type="text" {...register("nombre")} /></label>
                             </div>
                             {/* Apellido */}
                             <div className="labelApellido">
-                                <label htmlFor="apellido">Apellido</label>
-                                <input type="text" {...register("apellido")} />
+                                <label htmlFor="apellido">Apellido <input type="text" {...register("apellido")} /></label>
                             </div>
                         </div>
                         {/* Email */}
-                        <label htmlFor="email">Correo electrónico</label>
-                        <input type="email" {...register("email")} />
+                        <label htmlFor="email">Correo electrónico <input type="email" {...register("email")} /></label>
                         {/* Telefono */}
-                        <label htmlFor="telefono">Telefono</label>
-                        <input type="tel" {...register("telefono")} />
+                        <label htmlFor="telefono">Telefono <input type="tel" {...register("telefono")} /></label>
                         {/* Mensaje */}
-                        <label htmlFor="mensaje">Mensaje</label>
-                        <input type="text" {...register("mensaje")} />
+                        <label htmlFor="mensaje">Mensaje <input type="text" {...register("mensaje")} /></label>
                         <button className='buttonForm'>Enviar</button>
                     </form>
                 </div>
