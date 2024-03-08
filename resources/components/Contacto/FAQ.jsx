7782ebeffa90c0241cb1b2169154fa732e299be8
nahuek4 <nahuek.75@gmail.com>
2024-03-08 03:07:38 -0300

    a

--- resources/components/Contacto/FAQ.jsx
+++ resources/components/Contacto/FAQ.jsx
@@ -19,41 +19,33 @@ const FAQ = () => {
                         <h1>Preguntas <span className='spanRosa'>Frecuentes</span></h1>
                         <p>FAQS Generales</p>
                     </div>
-                    <div className="item">
-                        <div className="tittle" onClick={() => toggle(i)}>
-                            <div className="vacio"></div>
-                            <h1>¿Puedo diseñar solo el logo de mi marca?</h1>
-                            <span>{selected === i ? '-' : '+'}</span>
-                        </div>
-                        <div className={selected === i ? "content show" : "content"}><p>Hola</p></div>
+                    <div className="tittle" onClick={() => toggle(i)}>
+                        <div className="vacio"></div>
+                        <h1>¿Puedo diseñar solo el logo de mi marca?</h1>
+                        <span>{selected === i ? '-' : '+'}</span>
                     </div>
-                    <div className="item">
-                        <div className="tittle" onClick={() => toggle(i)}>
-                            <div className="vacio"></div>
-                            <h1>¿Cuáles son los métodos de pago de los servicios?</h1>
-                            <span>{selected === i ? '-' : '+'}</span>
-                        </div>
-                        <div className={selected === i ? "content show" : "content"}><p>Hola</p></div>
+                    <div className={selected === i ? "content show" : "content"}><p>hola</p></div>
+                    <div className="tittle" onClick={() => toggle(i)}>
+                        <div className="vacio"></div>
+                        <h1>¿Cuáles son los métodos de pago de los servicios?</h1>
+                        <span>{selected === i ? '-' : '+'}</span>
                     </div>
-                    <div className="item">
-                        <div className="tittle" onClick={() => toggle(i)}>
-                            <div className="vacio"></div>
-                            <h1>¿Cuánto demora del proceso de Branding?</h1>
-                            <span>{selected === i ? '-' : '+'}</span>
-                        </div>
-                        <div className={selected === i ? "content show" : "content"}><p>Hola</p></div>
+                    <div className={selected === i ? "content show" : "content"}><p>Hola</p></div>
+                    <div className="tittle" onClick={() => toggle(i)}>
+                        <div className="vacio"></div>
+                        <h1>¿Cuánto demora del proceso de Branding?</h1>
+                        <span>{selected === i ? '-' : '+'}</span>
                     </div>
-                    <div className="item">
-                        <div className="tittle" onClick={() => toggle(i)}>
-                            <div className="vacio"></div>
-                            <h1>¿En donde dan servicio?</h1>
-                            <span>{selected === i ? '-' : '+'}</span>
-                        </div>
-                        <div className={selected === i ? "content show" : "content"}><p>Hola</p></div>
+                    <div className={selected === i ? "content show" : "content"}><p>hola</p></div>
+                    <div className="tittle" onClick={() => toggle(i)}>
+                        <div className="vacio"></div>
+                        <h1>¿En donde dan servicio?</h1>
+                        <span>{selected === i ? '-' : '+'}</span>
                     </div>
+                    <div className={selected === i ? "content show" : "content"}><p>hola</p></div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
