04b5c724b9084a6b12fae438834d66ca54ed9ab8
nahuek4 <nahuek.75@gmail.com>
2024-03-09 01:11:51 -0300

    resize logo

--- resources/components/Inicio/Header.jsx
+++ resources/components/Inicio/Header.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import logob from '../../assets/img/logos/logopB.webp'
+import logopB from '../../assets/img/logos/logopB.webp'
+import logob from '../../assets/img/logos/logob.webp'
 import About from './About'
 import { Link } from 'react-router-dom'
 
 
 const Header = () => {
+    const [imagenSrc, setImagenSrc] = useState('imagen1.jpg');
+
+    useEffect(() => {
+
+        const cambiarImagenSegunAncho = () => {
+            if (window.innerWidth <= 500) {
+                setImagenSrc(logob);
+            } else {
+                setImagenSrc(logopB);
+            }
+        };
+
+        cambiarImagenSegunAncho();
+
+        window.addEventListener('resize', cambiarImagenSegunAncho);
+
+        return () => {
+            window.removeEventListener('resize', cambiarImagenSegunAncho);
+        };
+    }, []);
     return (
         <div>
             <div className='header'>
                 <div className="logo">
-                    <img className="logoBumik" src={logob} alt="logo bumik" />
+                    <img className="logoBumik" src={imagenSrc} alt="logo bumik" />
                     <h1 className='textHeader'>Estudio de diseño gráfico & desarrollo web</h1>
                 </div>
 
