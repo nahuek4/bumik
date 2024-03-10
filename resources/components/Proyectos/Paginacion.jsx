00a062e271ef15edd2898841df5f726c42d44908
nahuek4 <nahuek.75@gmail.com>
2024-03-10 18:24:06 -0300

    try

--- resources/components/Proyectos/Paginacion.jsx
+++ resources/components/Proyectos/Paginacion.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import ReactPaginate from 'react-paginate';
+import { getProyectos } from '../../services/getServices';
+import Paginacion from './Paginacion';
 
 const Paginacion = ({ datos }) => {
+    const [datos, setDatos] = useState([]);
     const [page, setPage] = useState(0);
     const [filterData, setFilterData] = useState();
     const n = 3;
@@ -12,13 +15,27 @@ const Paginacion = ({ datos }) => {
                 return (index >= page * n) & (index < (page + 1) * n);
             })
         );
+
+        const fetchData = async () => {
+            const data = await getProyectos();
+            setDatos(data);
+        };
+
+        fetchData();
     }, [page]);
 
     return (
         <div className="App">
-            <ul>
-                {filterData && filterData.map((item, index) => <li>Item #{item}</li>)}
-            </ul>
+            <div className="galleryContainComp">
+                <div className="galleryContain">
+                    {datos.map((image, index) => (
+                        <div key={index} className='galleryItem'>
+                            <img src={`assets/img/sliders/proyectos/${image.imagen}`} alt={image.alt} />
+                        </div>
+                    ))}
+                </div>
+                <Paginacion datos={datos} />
+            </div>
             <ReactPaginate
                 containerClassName={"pagination"}
                 activeClassName={"active"}
