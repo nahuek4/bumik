ff6db803cf2851ebbc86b2d91a1e1c0adbe5120b
nahuek4 <nahuek.75@gmail.com>
2024-03-10 18:24:38 -0300

    a

--- resources/components/Proyectos/Paginacion.jsx
+++ resources/components/Proyectos/Paginacion.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ReactPaginate from 'react-paginate';
 import { getProyectos } from '../../services/getServices';
-import Paginacion from './Paginacion';
 
 const Paginacion = ({ datos }) => {
     const [datos, setDatos] = useState([]);
