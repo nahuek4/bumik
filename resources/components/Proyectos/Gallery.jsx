9d8c2f1e07d0fe90805d93b88319d2158fdb5212
nahuek4 <nahuek.75@gmail.com>
2024-03-10 17:45:12 -0300

    paginacion

--- resources/components/Proyectos/Gallery.jsx
+++ resources/components/Proyectos/Gallery.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { getProyectos } from '../../services/getServices'
+import Paginacion from './Paginacion';
 
 
 const Gallery = () => {
     const [datos, setDatos] = useState([]);
+    const [pagina, setPagina] = useState(1);
+    const [porPagina, setPorPagina] = useState(6);
+
+    const maximo = datos.length / porPagina;
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,11 +24,12 @@ const Gallery = () => {
         <div>
             <div className="galleryContainComp">
                 <div className="galleryContain">
-                    {datos.map((image, index) => (
+                    {datos.slice((pagina - 1) * porPagina, (pagina - 1) * porPagina + porPagina).map((image, index) => (
                         <div key={index} className='galleryItem'>
                             <img src={`assets/img/sliders/proyectos/${image.imagen}`} alt={image.alt} />
                         </div>
                     ))}
+                    <Paginacion pagina={pagina} setPagina={setPagina} maximo={maximo} />
                 </div>
             </div>
         </div>
