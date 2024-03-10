d117a6b662cc8ce6fd69bb562f65b5d75ae3e1bc
nahuek4 <nahuek.75@gmail.com>
2024-03-10 19:21:42 -0300

    a

--- resources/components/Proyectos/Paginacion.jsx
+++ resources/components/Proyectos/Paginacion.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import ReactPaginate from 'react-paginate';
 import { getProyectos } from '../../services/getServices';
 
