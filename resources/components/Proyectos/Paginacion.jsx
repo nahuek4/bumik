c3e6dd31cfa399aa3fba19238f1e3b4caf94ad16
valentinzector <valentin@zector.com.ar>
2024-03-26 19:30:09 -0300

    fix test

--- resources/components/Proyectos/Paginacion.jsx
+++ resources/components/Proyectos/Paginacion.jsx
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import { getProyectos } from '../../services/getServices';
 
 const Paginacion = () => {
     const [datos, setDatos] = useState([]);
+    const [filterData, setFilterData] = useState([]);
     const [page, setPage] = useState(0);
-    const [filterData, setFilterData] = useState(0);
     const n = 4;
 
     useEffect(() => {
         const fetchData = async () => {
             const data = await getProyectos();
             setDatos(data);
-
-            setFilterData(
-                datos.filter((item, index) => {
-                    return (index >= page * n) & (index < (page + 1) * n);
-                })
-            );
         };
 
         fetchData();
-    }, [page]);
+    }, []);
+
+    useEffect(() => {
+        if (datos.length > 0) {
+            setFilterData(
+                datos.slice(page * n, (page + 1) * n)
+            );
+        }
+    }, [datos, page]);
 
     return (
         <div>
             <div className="galleryContainComp">
                 <div className="galleryContain">
-                    {filterData && filterData.map((image, index) => (
+                    {filterData.map((image, index) => (
                         <div key={index} className='galleryItem'>
                             <img src={`assets/img/sliders/proyectos/${image.imagen}`} alt={image.alt} />
                         </div>
