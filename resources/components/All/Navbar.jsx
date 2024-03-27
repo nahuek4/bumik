edf91d2ee25d9d3483dbbcd0d72ab4836a7fc594
valentinzector <valentin@zector.com.ar>
2024-03-26 21:49:22 -0300

    test

--- resources/components/All/Navbar.jsx
+++ resources/components/All/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 import "../../css/app.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark, } from '@fortawesome/free-solid-svg-icons';
@@ -9,14 +9,19 @@ const Navbar = () => {
 
     const handleClick = () => setClick(!click);
 
+    const isActive = useMatch({
+        path: '/',
+        exact: true
+    });
+
     return (
         <div className='navbar'>
             <div className="navMenu">
                 <ul className={click ? 'navLinks active' : 'navLinks'}>
-                    <li className='navItem'><NavLink className="navLink" exact to="/" onClick={handleClick} activeClassName="activeLink">Inicio</NavLink></li>
-                    <li className='navItem'><NavLink className="navLink" to="/servicios" onClick={handleClick} activeClassName="activeLink">Servicios</NavLink></li>
-                    <li className='navItem'><NavLink className="navLink" to="/portfolio" onClick={handleClick} activeClassName="activeLink">Proyectos</NavLink></li>
-                    <li className='navItem'><NavLink className="navLink" to="/contacto" onClick={handleClick} activeClassName="activeLink">Contacto</NavLink></li>
+                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/" onClick={handleClick}>Inicio</NavLink></li>
+                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/servicios" onClick={handleClick}>Servicios</NavLink></li>
+                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/portfolio" onClick={handleClick}>Proyectos</NavLink></li>
+                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/contacto" onClick={handleClick}>Contacto</NavLink></li>
                 </ul>
             </div>
             <div className="navToggle" onClick={handleClick}>
