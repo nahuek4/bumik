660526767a5b3b25c6fcd584fd7eeaef5fb4519e
nahuek4 <nahuek.75@gmail.com>
2024-03-27 00:57:56 -0300

    a

--- resources/components/Proyectos/Paginacion.jsx
+++ resources/components/Proyectos/Paginacion.jsx
@@ -32,11 +32,9 @@ const Paginacion = () => {
                     {filterData.map((image, index) => (
                         <div key={index} className='galleryItem'>
                             <img src={`/assets/img/sliders/proyectos/${image.imagen}`} alt={image.alt} />
-                            <a href={image.href}>
-                                <div className="content">
-                                    <h3>Ver en Behance</h3>
-                                </div>
-                            </a>
+                            <div className="content">
+                                <a href={image.href}><h3>Ver en Behance</h3></a>
+                            </div>
                         </div>
                     ))}
                 </div>
