d074c20d141d49e6016f97b282f215c3f0861d7d
valentinzector <valentin@zector.com.ar>
2024-03-26 21:51:17 -0300

    fix navbar

--- resources/components/All/Navbar.jsx
+++ resources/components/All/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { NavLink, useMatch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "../../css/app.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark, } from '@fortawesome/free-solid-svg-icons';
@@ -9,19 +9,14 @@ const Navbar = () => {
 
     const handleClick = () => setClick(!click);
 
-    const isActive = useMatch({
-        path: '/',
-        exact: true
-    });
-
     return (
         <div className='navbar'>
             <div className="navMenu">
                 <ul className={click ? 'navLinks active' : 'navLinks'}>
-                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/" onClick={handleClick}>Inicio</NavLink></li>
-                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/servicios" onClick={handleClick}>Servicios</NavLink></li>
-                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/portfolio" onClick={handleClick}>Proyectos</NavLink></li>
-                    <li className='navItem'><NavLink className={`navLink ${isActive ? 'activeLink' : ''}`} to="/contacto" onClick={handleClick}>Contacto</NavLink></li>
+                    <li className='navItem'><NavLink className="navLink" to="/" onClick={handleClick}>Inicio</NavLink></li>
+                    <li className='navItem'><NavLink className="navLink" to="/servicios" onClick={handleClick}>Servicios</NavLink></li>
+                    <li className='navItem'><NavLink className="navLink" to="/portfolio" onClick={handleClick}>Proyectos</NavLink></li>
+                    <li className='navItem'><NavLink className="navLink" to="/contacto" onClick={handleClick}>Contacto</NavLink></li>
                 </ul>
             </div>
             <div className="navToggle" onClick={handleClick}>
