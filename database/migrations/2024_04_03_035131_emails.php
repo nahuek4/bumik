ec3028b6adbc0e42ce622c225660cf94df07f3f5
valentinzector <valentin@zector.com.ar>
2024-04-03 00:53:34 -0300

    add migration

---
+++ database/migrations/2024_04_03_035131_emails.php
@@ -0,0 +1,39 @@
+
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->id();
+            $table->string('to_email');
+            $table->string('subject');
+            $table->text('body');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email');
+            $table->string('telefono')->nullable();
+            $table->text('mensaje');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
