786a2cdafd73b210fc40ed3e605ae2c526f7cbfd
valentinzector <valentin@zector.com.ar>
2024-04-05 00:11:36 -0300

    test

--- resources/components/Contacto/Form.jsx
+++ resources/components/Contacto/Form.jsx
@@ -35,35 +35,38 @@ const Form = () => {
 
     return (
         <div>
-            <div className="formContainer">
-                <div className="formContain">
-                    <form className="form" id='contactForm' onSubmit={handleSubmit(onSubmit)}>
-                        <div className="formNombreApellido">
-                            {/* nombre */}
-                            <div className="labelNombre">
-                                <label htmlFor="nombre">Nombre</label>
-                                <input type="text" {...register("nombre")} />
+            {!showAnimation && (
+                <div className="formContainer">
+                    <div className="formContain">
+                        <form className="form" id='contactForm' onSubmit={handleSubmit(onSubmit)}>
+                            <div className="formNombreApellido">
+                                {/* nombre */}
+                                <div className="labelNombre">
+                                    <label htmlFor="nombre">Nombre</label>
+                                    <input type="text" {...register("nombre")} />
+                                </div>
+                                {/* Apellido */}
+                                <div className="labelApellido">
+                                    <label htmlFor="apellido">Apellido</label>
+                                    <input type="text" {...register("apellido")} />
+                                </div>
                             </div>
-                            {/* Apellido */}
-                            <div className="labelApellido">
-                                <label htmlFor="apellido">Apellido</label>
-                                <input type="text" {...register("apellido")} />
-                            </div>
-                        </div>
-                        {/* Email */}
-                        <label htmlFor="email">Correo electrónico</label>
-                        <input type="email" {...register("email")} />
-                        {/* Telefono */}
-                        <label htmlFor="telefono">Telefono</label>
-                        <input type="tel" {...register("telefono")} />
-                        {/* Mensaje */}
-                        <label htmlFor="mensaje">Mensaje</label>
-                        <textarea rows="8" type="text" {...register("mensaje")} />
-                        <button className='buttonForm'>Enviar</button>
-                        <input type="hidden" name="_token" value={csrfToken} />
-                    </form>
+                            {/* Email */}
+                            <label htmlFor="email">Correo electrónico</label>
+                            <input type="email" {...register("email")} />
+                            {/* Telefono */}
+                            <label htmlFor="telefono">Telefono</label>
+                            <input type="tel" {...register("telefono")} />
+                            {/* Mensaje */}
+                            <label htmlFor="mensaje">Mensaje</label>
+                            <textarea rows="8" type="text" {...register("mensaje")} />
+                            <button className='buttonForm'>Enviar</button>
+                            <input type="hidden" name="_token" value={csrfToken} />
+                        </form>
+                    </div>
                 </div>
-            </div>
+            )}
+            {showAnimation && <Lottie animationData={LottieSuccess} />}
             <div className="textButton">
                 <p>¿Tenes alguna consulta de cómo podrías llevar Tu proyecto adelante?</p>
                 <p>Envíanos un mensaje a estudiobumik@gmail.com o completá el formulario</p>
@@ -72,7 +75,6 @@ const Form = () => {
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSdZL63YOXJUQ2Ee7VYszlfoi8bPjeuZvujjGCmt-j_ewmTi5A/viewform?usp=sf_link" target="_blank" rel="noreferrer"><button className='botonServicios'>¡Cotizá tu proyecto!</button></a>
                 </div>
             </div>
-            {showAnimation && <Lottie animationData={LottieSuccess} />}
             <FAQ />
         </div>
     );
