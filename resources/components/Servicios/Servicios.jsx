e78263fe987f64019593c2a0a84c2fc8e04ba461
nahuek4 <nahuek.75@gmail.com>
2024-04-06 19:15:32 -0300

    test

--- resources/components/Servicios/Servicios.jsx
+++ resources/components/Servicios/Servicios.jsx
@@ -15,18 +15,26 @@ const Servicios = () => {
     }
 
     function datosTipo(tipo) {
-        return datos.filter(item => item.tipo === tipo).map((item, i) => {
-            return (
-                <div className="item" key={i}>
-                    <div className="tittle" onClick={() => toggle(i)}>
-                        <div className="vacio"></div>
-                        <h1>{item.titulo}</h1>
-                        <span className='spanVerde'>{selected === i ? '-' : '+'}</span>
+        return datos
+            .filter(item => item.tipo === tipo)
+            .map((item, i) => {
+                const descripcionItems = item.descripcion.split('<br>').map((punto, index) => (
+                    <li key={index}>{punto.trim()}</li>
+                ));
+
+                return (
+                    <div className="item" key={i}>
+                        <div className="tittle" onClick={() => toggle(i)}>
+                            <div className="vacio"></div>
+                            <h1>{item.titulo}</h1>
+                            <span className='spanVerde'>{selected === i ? '-' : '+'}</span>
+                        </div>
+                        <div className={selected === i ? "content show" : "content"}>
+                            <ul>{descripcionItems}</ul>
+                        </div>
                     </div>
-                    <div className={selected === i ? "content show" : "content"}><p>{item.descripcion}</p></div>
-                </div>
-            );
-        });
+                );
+            });
     }
 
     useEffect(() => {
