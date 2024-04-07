33af6d9356451830da5070b0a058c5b719690cc2
valentinzector <valentin@zector.com.ar>
2024-04-07 16:16:00 -0300

    test

--- resources/components/Contacto/Form.jsx
+++ resources/components/Contacto/Form.jsx
@@ -3,11 +3,11 @@ import { useForm } from 'react-hook-form';
 import FAQ from './FAQ';
 import Lottie from 'lottie-react';
 import LottieSuccess from '../../assets/img/lottie/success.json';
-import Loader from '../../assets/icons/ajax-loader.gif';
+import Loader from '../../assets/img/loader.gif';
 
 const Form = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const [csrfToken, setCsrfToken] = useState('');
     const [showAnimation, setShowAnimation] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
@@ -20,23 +20,25 @@ const Form = () => {
     }, []);
 
     const onSubmit = async (data) => {
-        try {
-            setIsLoading(true);
-            const response = await fetch('/submitForm', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': csrfToken
-                },
-                body: JSON.stringify(data)
-            });
+        if (!Object.keys(errors).length) {
+            try {
+                setIsLoading(true);
+                const response = await fetch('/submitForm', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': csrfToken
+                    },
+                    body: JSON.stringify(data)
+                });
 
-            const responseData = await response.json();
-            setShowAnimation(true);
-            setIsLoading(false);
-        } catch (error) {
-            console.error('Error al enviar la solicitud:', error);
-            setIsLoading(false);
+                const responseData = await response.json();
+                setShowAnimation(true);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error al enviar la solicitud:', error);
+                setIsLoading(false);
+            }
         }
     };
 
@@ -51,24 +53,29 @@ const Form = () => {
                                 {/* nombre */}
                                 <div className="labelNombre">
                                     <label htmlFor="nombre">Nombre</label>
-                                    <input type="text" {...register("nombre")} />
+                                    <input type="text" {...register("nombre", { required: true })} />
+                                    {errors.nombre && <span className="error">Este campo es requerido</span>}
                                 </div>
                                 {/* Apellido */}
                                 <div className="labelApellido">
                                     <label htmlFor="apellido">Apellido</label>
-                                    <input type="text" {...register("apellido")} />
+                                    <input type="text" {...register("apellido", { required: true })} />
+                                    {errors.apellido && <span className="error">Este campo es requerido</span>}
                                 </div>
                             </div>
                             {/* Email */}
                             <label htmlFor="email">Correo electrónico</label>
-                            <input type="email" {...register("email")} />
+                            <input type="email" {...register("email", { required: true })} />
+                            {errors.email && <span className="error">Este campo es requerido</span>}
                             {/* Telefono */}
                             <label htmlFor="telefono">Telefono</label>
-                            <input type="tel" {...register("telefono")} />
+                            <input type="tel" {...register("telefono", { required: true })} />
+                            {errors.telefono && <span className="error">Este campo es requerido</span>}
                             {/* Mensaje */}
                             <label htmlFor="mensaje">Mensaje</label>
-                            <textarea rows="8" type="text" {...register("mensaje")} />
-                            <button className='buttonForm' id='buttonForm' disabled={isLoading} onClick={() => setIsLoading(true)}>
+                            <textarea rows="8" type="text" {...register("mensaje", { required: true })} />
+                            {errors.mensaje && <span className="error">Este campo es requerido</span>}
+                            <button className='buttonForm' id='buttonForm' disabled={isLoading}>
                                 {isLoading ? <img src={Loader} alt="Loading..." /> : 'Enviar'}
                             </button>
                             <input type="hidden" name="_token" value={csrfToken} />
