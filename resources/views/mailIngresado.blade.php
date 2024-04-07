8cdb314caf7f1a697a0b15e89195bf5c23a7f04c
valentinzector <valentin@zector.com.ar>
2024-04-07 02:40:44 -0300

    test

--- resources/views/mailIngresado.blade.php
+++ resources/views/mailIngresado.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bumik Mailer</title>
+    <title>Recibimos tu consulta</title>
 </head>
 <body>
     <img src="{{ $message->embed(public_path().'/assets/img/mailing.png') }}">
