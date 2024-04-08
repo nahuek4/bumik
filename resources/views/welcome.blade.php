48de1ca08e60e0e78d7035dcd9f0e6fcc7379d4a
valentinzector <valentin@zector.com.ar>
2024-04-08 20:21:48 -0300

    fix test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="assets/img/logos/logowminimoB.webp" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="assets/img/logos/logowminimoB.webp" type="image/x-icon"> -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Bumik</title>
 </head>
 <body>
