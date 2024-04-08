0a22e937ed4a984163f3fc548d4258e1ad1d6d55
nahuek4 <nahuek.75@gmail.com>
2024-04-08 20:20:55 -0300

    test

--- resources/components/Inicio/Services.jsx
+++ resources/components/Inicio/Services.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getServices } from '../../services/getServices';
 import Works from './Works';
+import { Slide } from "react-awesome-reveal";
 
 const Services = () => {
     const [datos, setDatos] = useState([]);
@@ -16,38 +17,41 @@ const Services = () => {
 
     return (
         <div className="serviceComp">
-            <div className="serviceBackground">
-                <div className='servicesContain'>
-                    <div className="serviciosText">
-                        <h1>Nuestros <span className='spanVerde'>Servicios</span></h1>
-                        <p>What we do</p>
-                    </div>
-                    <div className='serviciosGen'>
-                        <div className="serviceContainer">
-                            {datos.map((servicio, index) => (
-                                <div className="boxBorder" key={index}>
-                                    <div className="serviceBox">
-                                        <ul>
-                                            <li className='liImg'>
-                                                <img src={`../../assets/icons/${servicio.imagen}`} alt="servicio ofrecido" />
-                                            </li>
-                                            <li className='liServices'>{servicio.servicio}</li>
-                                            <li className='liTextServices'>
-                                                {servicio.descripcion.split('\n').map((line, lineIndex) => (
-                                                    <p key={lineIndex}>{line}</p>
-                                                ))}
-                                            </li>
-                                        </ul>
+            <Slide direction='up' duration="1500" triggerOnce>
+                <div className="serviceBackground">
+                    <div className='servicesContain'>
+                        <div className="serviciosText">
+                            <h1>Nuestros <span className='spanVerde'>Servicios</span></h1>
+                            <p>What we do</p>
+                        </div>
+                        <div className='serviciosGen'>
+                            <div className="serviceContainer">
+                                {datos.map((servicio, index) => (
+                                    <div className="boxBorder" key={index}>
+                                        <div className="serviceBox">
+                                            <ul>
+                                                <li className='liImg'>
+                                                    <img src={`../../assets/icons/${servicio.imagen}`} alt="servicio ofrecido" />
+                                                </li>
+                                                <li className='liServices'>{servicio.servicio}</li>
+                                                <li className='liTextServices'>
+                                                    {servicio.descripcion.split('\n').map((line, lineIndex) => (
+                                                        <p key={lineIndex}>{line}</p>
+                                                    ))}
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
+                        </div>
+                        <div className="botonService">
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdZL63YOXJUQ2Ee7VYszlfoi8bPjeuZvujjGCmt-j_ewmTi5A/viewform?usp=sf_link" target="_blank" rel="noreferrer"><button className='botonServicios'>¡Cotizá tu proyecto!</button></a>
                         </div>
                     </div>
-                    <div className="botonService">
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdZL63YOXJUQ2Ee7VYszlfoi8bPjeuZvujjGCmt-j_ewmTi5A/viewform?usp=sf_link" target="_blank" rel="noreferrer"><button className='botonServicios'>¡Cotizá tu proyecto!</button></a>
-                    </div>
                 </div>
-            </div>
+            </Slide>
+
             <Works />
         </div>
     );
