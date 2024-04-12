3cc4a2ef87a766485cccda9907bb1bd9f6c8ed70
nahuek4 <nahuek.75@gmail.com>
2024-04-12 20:36:14 -0300

    test

--- resources/components/Inicio/About.jsx
+++ resources/components/Inicio/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
                             type: 'fraction',
                         }}
                         autoplay={{
-                            delay: 2500,
+                            delay: 4000,
                             disableOnInteraction: false,
                         }}
                         rewind={true}
