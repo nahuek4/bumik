1a5273ca8adb957f9009d9aa39646e9287584b78
nahuek4 <nahuek.75@gmail.com>
2024-04-24 19:35:42 -0300

    test

--- resources/components/Inicio/About.jsx
+++ resources/components/Inicio/About.jsx
@@ -29,6 +29,7 @@ const About = () => {
                 </div>
                 <div className="perfiles">
                     <Swiper
+                        effect='coverflow'
                         slidesPerView={3}
                         centeredSlides={true}
                         spaceBetween={30}
