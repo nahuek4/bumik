6e3a84e6abaeb5648e5d81b6a8521442a876e752
nahuek4 <nahuek.75@gmail.com>
2024-04-24 22:03:38 -0300

    .

--- resources/components/Inicio/About.jsx
+++ resources/components/Inicio/About.jsx
@@ -66,7 +66,6 @@ const About = () => {
                 <div className="perfiles">
                     <Slider {...settings} className='sliderSettings'>
                         {datos.map((equipo, index) => (
-
                             <div className="perfil" key={index}>
                                 <div className="fondoPerfil">
                                     <div className="imgPerfil"><img src={`assets/img/fotos${equipo.foto}.webp`} alt="Foto de perfil" /></div>
