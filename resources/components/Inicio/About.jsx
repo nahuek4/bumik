1d8f568b7a4dfd331d4df60b18658bd86ab9c738
nahuek4 <nahuek.75@gmail.com>
2024-04-24 21:19:12 -0300

    test

--- resources/components/Inicio/About.jsx
+++ resources/components/Inicio/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Banner from './Banner'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
-import "swiper/css/effect-coverflow"
-import 'swiper/css';
-import 'swiper/css/bundle';
+
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { getEquipo } from '../../services/getServices';
 import { useState, useEffect } from 'react';
 
@@ -18,7 +18,44 @@ const About = () => {
         };
 
         fetchData();
+
     }, []);
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        adaptiveWidth: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
     return (
         <div className='nosotros'>
             <div className="nosotrosContain">
@@ -53,17 +90,16 @@ const About = () => {
                         className="mySwiper"
                     >
                         {datos.map((equipo, index) => (
-                            <SwiperSlide>
-                                <div className="perfil" key={index}>
-                                    <div className="fondoPerfil">
-                                        <div className="imgPerfil"><img src={`assets/img/fotos${equipo.foto}.webp`} alt="Foto de perfil" /></div>
-                                        <div className="descripcion">
-                                            <h2>{equipo.nombre}</h2>
-                                            <p>{equipo.trabajo}</p>
-                                        </div>
+                            <Slider {...settings} className='sliderSettings'>                                <div className="perfil" key={index}>
+                                <div className="fondoPerfil">
+                                    <div className="imgPerfil"><img src={`assets/img/fotos${equipo.foto}.webp`} alt="Foto de perfil" /></div>
+                                    <div className="descripcion">
+                                        <h2>{equipo.nombre}</h2>
+                                        <p>{equipo.trabajo}</p>
                                     </div>
                                 </div>
-                            </SwiperSlide>
+                            </div>
+                            </Slider>
                         ))}
                     </Swiper>
                 </div>
