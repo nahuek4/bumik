98e764d6905ed5de8c340c2688de24e985e5b97a
nahuek4 <nahuek.75@gmail.com>
2024-05-04 20:34:40 -0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
-    <link rel="shortcut icon" href="assets/img/logos/logowminimoB.webp" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/img/logos/logowminimo.webp" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Estudio Bumik</title>
 </head>
