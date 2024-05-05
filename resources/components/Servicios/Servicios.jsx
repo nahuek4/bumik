5592c61c312ad4cbe0c498e6dbf950a547b0e0e4
nahuek4 <nahuek.75@gmail.com>
2024-05-04 21:00:56 -0300

    test

--- resources/components/Servicios/Servicios.jsx
+++ resources/components/Servicios/Servicios.jsx
@@ -84,74 +84,86 @@ const Servicios = () => {
             </div>
             <div className="serviciosContain">
                 <div className="serviceServicio">
-                    <div className="serviceServicioimg">
-                        <div className=""><img src="/assets/img/fotos/pcmuestra.webp" alt="pc muestra" /></div>
-                    </div>
-                    <div className="containerData">
-                        <div className="tituloServicio">
-                            <h1 className='spanVerde'>Diseño & Desarrollo web</h1>
-                            <p className='descripcionServicio'> Te ofrecemos un servicio completo de diseño y desarrolllo web para ayudarte a crear una presencia online única y efectiva para tu marca o negocio. Trabajamos con vos para crear algo personalizado y profesional que refleje tu identidad y valores de marca.</p>
-                            <p className='descripcionServicio'> Además, nos aseguramos que la página web sea fácil de usar, rápida y segura para mejorar la experiencia del usuario y aumentar la conversión de clientes potenciales.</p>
-                            <p className='descripcionServicio'>Ya sea que necesites una página web institucional, un portfolio, una tienda online o una landing page, estamos para ayudarte a crear una presencia que destaque en un mercado competitivo.</p>
+                    <Slide duration="1800" triggerOnce fraction="0">
+                        <div className="serviceServicioimg">
+                            <div className=""><img src="/assets/img/fotos/pcmuestra.webp" alt="pc muestra" /></div>
                         </div>
-                        <div className="mostrarMas">
-                            <div className="column">
-                                {datosTipo(datos, "desarrollo", selected, toggle)}
+                    </Slide>
+                    <Slide duration="1800" triggerOnce direction='right' fraction="0">
+                        <div className="containerData">
+                            <div className="tituloServicio">
+                                <h1 className='spanVerde'>Diseño & Desarrollo web</h1>
+                                <p className='descripcionServicio'> Te ofrecemos un servicio completo de diseño y desarrolllo web para ayudarte a crear una presencia online única y efectiva para tu marca o negocio. Trabajamos con vos para crear algo personalizado y profesional que refleje tu identidad y valores de marca.</p>
+                                <p className='descripcionServicio'> Además, nos aseguramos que la página web sea fácil de usar, rápida y segura para mejorar la experiencia del usuario y aumentar la conversión de clientes potenciales.</p>
+                                <p className='descripcionServicio'>Ya sea que necesites una página web institucional, un portfolio, una tienda online o una landing page, estamos para ayudarte a crear una presencia que destaque en un mercado competitivo.</p>
+                            </div>
+                            <div className="mostrarMas">
+                                <div className="column">
+                                    {datosTipo(datos, "desarrollo", selected, toggle)}
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Slide>
                 </div>
             </div>
             <div className="serviciosContain">
                 <div className="serviceServicio">
-                    <div className="serviceServicioimg">
-                        <div className=""><img src="/assets/img/fotos/celulares.webp" alt="celulares muestra" /></div>
-                    </div>
-                    <div className="containerData">
-                        <div className="tituloServicio">
-                            <h1 className='spanVerde'>Social Media</h1>
-                            <p className='descripcionServicio'> En el mundo actual, las redes sociales son fundamentales para dar a conocer tu marca. En Bumik entendemos que es importante no sólo tener una presencia online, sino destacarse visualmente, ser coherente y transmitir los valores de la marca. Creando así una comunidad real y fiel. </p>
-                            <p className='descripcionServicio'> ¡En Bumik te ayudamos a crear una presencia digital sólida y atractiva que te conecte con tus clientes!</p>
+                    <Slide duration="1800" triggerOnce fraction="0">
+                        <div className="serviceServicioimg">
+                            <div className=""><img src="/assets/img/fotos/celulares.webp" alt="celulares muestra" /></div>
                         </div>
-                        <div className="mostrarMas">
-                            <div className="column">
-                                <div className="columns">
-                                    <h2>Posteos/historias mensuales</h2>
-                                    <h2>Diseño de feed</h2>
-                                    <h2>Plantillas editables</h2>
-                                    <h2>Edición de fotografías</h2>
-                                    <h2>Reels/Edición de videos</h2>
+                    </Slide>
+                    <Slide duration="1800" triggerOnce direction='right' fraction="0">
+                        <div className="containerData">
+                            <div className="tituloServicio">
+                                <h1 className='spanVerde'>Social Media</h1>
+                                <p className='descripcionServicio'> En el mundo actual, las redes sociales son fundamentales para dar a conocer tu marca. En Bumik entendemos que es importante no sólo tener una presencia online, sino destacarse visualmente, ser coherente y transmitir los valores de la marca. Creando así una comunidad real y fiel. </p>
+                                <p className='descripcionServicio'> ¡En Bumik te ayudamos a crear una presencia digital sólida y atractiva que te conecte con tus clientes!</p>
+                            </div>
+                            <div className="mostrarMas">
+                                <div className="column">
+                                    <div className="columns">
+                                        <h2>Posteos/historias mensuales</h2>
+                                        <h2>Diseño de feed</h2>
+                                        <h2>Plantillas editables</h2>
+                                        <h2>Edición de fotografías</h2>
+                                        <h2>Reels/Edición de videos</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Slide>
                 </div>
             </div>
             <div className="serviciosContain">
                 <div className="serviceServicio">
-                    <div className="serviceServicioimg">
-                        <div className=""><img src="/assets/img/fotos/mateq.webp" alt="mateq packaging" /></div>
-                    </div>
-                    <div className="containerData">
-                        <div className="tituloServicio">
-                            <h1 className='spanVerde'>Packaging & Papelería</h1>
-                            <p className='descripcionServicio'> Combinamos creatividad y funcionalidad para presentar tu producto de manera atractiva, dejando una impresión duradera en tus consumidores. Te garantizamos que tu producto destaque con una mezcla única de estilo y utilidad.</p>
+                    <Slide duration="1800" triggerOnce fraction="0">
+                        <div className="serviceServicioimg">
+                            <div className=""><img src="/assets/img/fotos/mateq.webp" alt="mateq packaging" /></div>
                         </div>
-                        <div className="mostrarMas">
-                            <div className="column">
-                                <div className="columns">
-                                    <h2>Tarjetas personales</h2>
-                                    <h2>Etiquetas de producto</h2>
-                                    <h2>Stickers</h2>
-                                    <h2>Patterns</h2>
-                                    <h2>Cajas</h2>
-                                    <h2>Menús</h2>
-                                    <h2>Invitaciones</h2>
-                                    <h2>Y más</h2>
+                    </Slide>
+                    <Slide duration="1800" triggerOnce direction='right' fraction="0">
+                        <div className="containerData">
+                            <div className="tituloServicio">
+                                <h1 className='spanVerde'>Packaging & Papelería</h1>
+                                <p className='descripcionServicio'> Combinamos creatividad y funcionalidad para presentar tu producto de manera atractiva, dejando una impresión duradera en tus consumidores. Te garantizamos que tu producto destaque con una mezcla única de estilo y utilidad.</p>
+                            </div>
+                            <div className="mostrarMas">
+                                <div className="column">
+                                    <div className="columns">
+                                        <h2>Tarjetas personales</h2>
+                                        <h2>Etiquetas de producto</h2>
+                                        <h2>Stickers</h2>
+                                        <h2>Patterns</h2>
+                                        <h2>Cajas</h2>
+                                        <h2>Menús</h2>
+                                        <h2>Invitaciones</h2>
+                                        <h2>Y más</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Slide>
                 </div>
             </div>
             <div className="botonService">
