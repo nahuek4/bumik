41f45f14e250f3c052a2c001945dc7464c4c6060
nahuek4 <nahuek.75@gmail.com>
2024-05-04 22:08:27 -0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="">
+    <meta name="description" content="Trabajamos la identidad visual de tu marca, tu presencia en redes sociales y en el diseño y desarrolo web de tu sitio web. ¡Contactanos y cotizá tu proyecto!">
     <link rel="icon" href="assets/img/logos/logowminimo.webp" type="image/x-icon">
     <link rel="apple-touch-icon image_src" href="assets/img/logos/logowminimo.webp" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Estudio Bumik</title>
+    <title>Estudio Bumik | Diseño y desarrollo web</title>
 </head>
 <body>
     
