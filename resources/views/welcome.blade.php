83b55f973c1d594326adbffc17537c80564972d0
nahuek4 <nahuek.75@gmail.com>
2024-05-11 04:48:45 -0300

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Trabajamos la identidad visual de tu marca, tu presencia en redes sociales y en el diseño y desarrolo web de tu sitio web. ¡Contactanos y cotizá tu proyecto!">
+    <meta name="description" content="Estudio de diseño y desarrollo web. Trabajamos la identidad visual de tu marca, tu presencia en redes sociales y en el diseño y desarrolo web de tu sitio web. ¡Contactanos y cotizá tu proyecto!">
     <link rel="icon" href="assets/img/logos/logowminimo.webp" type="image/x-icon">
     <link rel="apple-touch-icon image_src" href="assets/img/logos/logowminimo.webp" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
